758657c0dd4809741abe3051c30b9e632d72980d
Taras Muryn <taras.muryn@sethq.com>
2013-09-26 18:00:12 +0300

    initial commit of the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FristApp::Application.config.secret_key_base = '08fa5ed557b944eb9b9416696540cf320a1fcc1d7402e61555ff2460246bcc9eee2aacc0bc71473cdf858b9ee1fa58aae947152809c9855946d2987b8d9a7615'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FristApp::Application.config.session_store :cookie_store, key: '_frist_app_session'
